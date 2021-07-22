00a225d11b91b0aadf233a0d4b4839b35342b338
Angad Kapoor <51783838+xAngad@users.noreply.github.com>
2021-07-22 14:43:51 +0530

    Created Error embed, commented unused functions, formatted code

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,15 +2,6 @@ import discord
 import numpy as np
 from mojang import MojangAPI
 
-def swXPtoLVL(xp):
-    xps = [0, 20, 70, 150, 250, 500, 1000, 2000, 3500, 6000, 10000, 15000]
-    if xp >= 15000:
-        return (xp - 15000)/10000 + 12
-    else:
-        for i in range(len(xps)):
-            if xp < xps[i]:
-                return i + float(xp - xps[i-1]) / (xps[i] - xps[i-1])
-
 def toPercentage(victories, defeats):
     return (victories / (victories + defeats))
 
@@ -37,55 +28,71 @@ def createEmbed(p1, p2):
     p1_probs = playerStats(p1)
     p2_probs = playerStats(p2)
 
-    sw_probs = np.round(winner(p1_probs["sw"], p2_probs["sw"]), 2)
-    bw_probs = np.round(winner(p1_probs["bw"], p2_probs["bw"]), 2)
-    duels_probs = np.round(winner(p1_probs["duels"], p2_probs["duels"]), 2)
+    sw_probs = winner(p1_probs["sw"], p2_probs["sw"])
+    bw_probs = winner(p1_probs["bw"], p2_probs["bw"])
+    duels_probs = winner(p1_probs["duels"], p2_probs["duels"])
 
     sw_winner = IGNs[np.argmax(sw_probs)]
     bw_winner = IGNs[np.argmax(bw_probs)]
     duels_winner = IGNs[np.argmax(duels_probs)]
 
     # Initialization
-    embed = discord.Embed(
-        # title="1v1",
-        # description=f"1v1 between {p1} and {p2}",
-        # url="https://25karma.xyz/",
-        type="rich",
-        color=discord.Color.gold()
-    )
+    embed = discord.Embed(type="rich", color=discord.Color.gold())
+
     # Author (comment out most probably)
-    embed.set_author(
-        name="HyChance - 1v1 Win Predictor",
-        # url="https://github.com/xAngad",
-        icon_url="https://crafatar.com/avatars/6327b3fb426b4b6a92fba78e13173a22?size=400"
-    )
+    embed.set_author(name="HyChance - 1v1 Win Predictor",
+                     icon_url="https://crafatar.com/avatars/6327b3fb426b4b6a92fba78e13173a22?size=400")
 
     # Thumbnail
-    embed.set_thumbnail(
-        url="https://assets.change.org/photos/8/bv/gd/yuBVGDPtWevQvmQ-800x450-noPad.jpg?1571761596"
-    )
+    embed.set_thumbnail(url="https://assets.change.org/photos/8/bv/gd/yuBVGDPtWevQvmQ-800x450-noPad.jpg?1571761596")
 
     # Stats fields
     embed.add_field(
         name="SkyWars",
-        value=f"`{sw_winner}` (`{sw_probs.max()*100}% chance`)",
+        value=f"`{sw_winner}` (`{round(max(sw_probs)*100, 2)}% chance`)",
         inline=False
     )
     embed.add_field(
         name="Bedwars",
-        value=f"`{bw_winner}` (`{bw_probs.max()*100}% chance`)",
+        value=f"`{bw_winner}` (`{round(max(bw_probs)*100, 2)}% chance`)",
         inline=False
     )
     embed.add_field(
         name="Duels",
-        value=f"`{duels_winner}` (`{duels_probs.max()*100}% chance`)",
+        value=f"`{duels_winner}` (`{round(max(duels_probs)*100, 2)}% chance`)",
         inline=False
     )
 
     # Footer w contact me
-    embed.set_footer(
-        text="See any bugs? DM me: xAngad#4229"
-    )
+    embed.set_footer(text="See any bugs? DM me: xAngad#4229" )
 
 
     return embed
+
+def createErrorEmbed():
+    embed = discord.Embed(type="rich", color=discord.Color.red())
+
+    embed.set_author(name="HyChance - 1v1 Win Predictor",
+                     icon_url="https://crafatar.com/avatars/6327b3fb426b4b6a92fba78e13173a22?size=400")
+
+    embed.set_thumbnail(url="https://www.freeiconspng.com/uploads/error-icon-4.png")
+
+    embed.add_field(
+        name="Error",
+        value="Please use format: `!duel <Player #1> <Player #2>`",
+        inline=False
+    )
+
+    embed.set_footer(text="See any bugs? DM me: xAngad#4229")
+
+    return embed
+
+
+# def swXPtoLVL(xp):
+#     xps = [0, 20, 70, 150, 250, 500, 1000, 2000, 3500, 6000, 10000, 15000]
+#     if xp >= 15000:
+#         return (xp - 15000)/10000 + 12
+#     else:
+#         for i in range(len(xps)):
+#             if xp < xps[i]:
+#                 return i + float(xp - xps[i-1]) / (xps[i] - xps[i-1])
