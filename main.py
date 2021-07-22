7ed088d3c787cebde84d20522a9ff844dcfd0848
Angad Kapoor <51783838+xAngad@users.noreply.github.com>
2021-07-22 15:09:28 +0530

    Removed HyChance class, added Activity Status

--- main.py
+++ main.py
@@ -5,32 +5,31 @@ load_dotenv()
 
 from scripts.utils import createEmbed, createErrorEmbed
 
-class HyChance(discord.Client, discord.Embed):
-    def __init__(self):
-        super().__init__()
-        self.aliases = ["!duel", "!d", "!1v1"]
+client = discord.Client()
+aliases = ["!duel", "!d", "!1v1"]
 
-    async def on_ready(self):
-        print("~~~~~~~~~~")
-        print(f"Bot logged in as: {self.user.name}")
-        print("~~~~~~~~~~")
+@client.event
+async def on_ready():
+    print(f"Bot logged in as: {client.user}")
+    print("Running...")
 
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("!duel"))
 
-        words = message.content.split()
-        if words[0] in self.aliases:
-            if len(words) == 3:
-                p1, p2 = words[1], words[2]
+@client.event
+async def on_message(message):
+    if message.author.id == client.user.id:
+        return
 
-                embedStats = createEmbed(p1, p2)
-                await message.channel.send(embed=embedStats)
+    words = message.content.split()
+    if words[0] in aliases:
+        if len(words) == 3:
+            p1, p2 = words[1], words[2]
 
-            else:
-                embedError = createErrorEmbed()
-                await message.channel.send(embed=embedError)
+            embedStats = createEmbed(p1, p2)
+            await message.channel.send(embed=embedStats)
 
+        else:
+            embedError = createErrorEmbed()
+            await message.channel.send(embed=embedError)
 
-client = HyChance()
 client.run(os.environ["TOKEN"])
